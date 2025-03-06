d647ba0378758319d2a9855032a2b6100020f848
Joshua Seidel <29733828+JoshuaSeidel@users.noreply.github.com>
2025-03-06 13:05:42 -0500

    Update provider.tf

--- mattermost/provider.tf
+++ mattermost/provider.tf
@@ -1,33 +1,46 @@
 terraform {
-  required_version = "~> 1.0"
+  required_version = "1.10.5"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = ">= 4.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
-    }
-    
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.4.1"
+      source  = "hashicorp/aws"
+      version = "~> 5.89.0"
     }
   }
 }
 
 provider "aws" {
-    region = "us-east-2"
+  alias   = "govcloud"
+  profile = local.vars.gcprofile
+  region  = local.vars.aws_region
+
+  use_fips_endpoint = true
+  default_tags {
+    tags = merge(local.vars.tags, {
+      Environment = "dev",
+      Project     = local.vars.project_name
+    })
+  }
+}
+
+provider "aws" {
+  alias   = "triafed"
+  profile = local.vars.profile
+  region  = "us-east-1"
+
+  default_tags {
+    tags = merge(local.vars.tags, {
+      Environment = "dev",
+      Project     = local.vars.project_name
+    })
+  }
 }
 
 provider "kubernetes" {
-    config_path = "/home/ubuntu/.kube/config" #change user as needed to match the user within the instance being used to deploy the DBZ production cluster
+    config_path = "~/.kube/config" #change user as needed to match the user within the instance being used to deploy the DBZ production cluster
   }
 
 provider "helm" {
   kubernetes {
-    config_path = "/home/ubuntu/.kube/config"
+    config_path = "~/.kube/config"
   }
 }
