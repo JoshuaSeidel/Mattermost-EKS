43aebd92bbd6150ae8fa4264e7d2208209c5ade9
Joshua Seidel <29733828+JoshuaSeidel@users.noreply.github.com>
2025-03-06 13:04:26 -0500

    Update mattermost.tf

--- mattermost/mattermost.tf
+++ mattermost/mattermost.tf
@@ -2,7 +2,7 @@ resource "helm_release" "mattermost" {
   name       = "mattermost"
   namespace = "mattermost"
   repository = "https://helm.mattermost.com"
-  chart      = "mattermost-team-edition"
+  chart      = "mattermost-enterprise-edition"
 
   values = [
     "${file("mattermost-values.yml")}"
