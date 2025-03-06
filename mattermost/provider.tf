779a54d2786ddc9ce38db91f428de504a198ba06
Joshua Seidel <29733828+JoshuaSeidel@users.noreply.github.com>
2025-03-06 13:08:30 -0500

    Update provider.tf

--- mattermost/provider.tf
+++ mattermost/provider.tf
@@ -1,9 +1,18 @@
 terraform {
-  required_version = "1.10.5"
+  required_version = "~> 1.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.89.0"
+        source = "hashicorp/aws"
+        version = "= 5.89.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
     }
   }
 }
