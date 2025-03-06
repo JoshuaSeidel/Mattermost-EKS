87d1dc69956163b2772e2ab5d859d0b5af0dece6
Joshua Seidel <29733828+JoshuaSeidel@users.noreply.github.com>
2025-03-06 13:08:46 -0500

    Update provider.tf

--- ingress/provider.tf
+++ ingress/provider.tf
@@ -3,19 +3,53 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = ">= 4.0"
+        version = "= 5.89.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.10"
     }
+    
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
   }
 }
 
 provider "aws" {
-    region = "us-east-2"
+  alias   = "govcloud"
+  profile = local.vars.gcprofile
+  region  = local.vars.aws_region
+
+  use_fips_endpoint = true
+  default_tags {
+    tags = merge(local.vars.tags, {
+      Environment = "dev",
+      Project     = local.vars.project_name
+    })
+  }
+}
+
+provider "aws" {
+  alias   = "triafed"
+  profile = local.vars.profile
+  region  = "us-east-1"
+
+  default_tags {
+    tags = merge(local.vars.tags, {
+      Environment = "dev",
+      Project     = local.vars.project_name
+    })
+  }
 }
 
 provider "kubernetes" {
-    config_path = "/home/ubuntu/.kube/config"
+    config_path = "~/.kube/config" #change user as needed to match the user within the instance being used to deploy the DBZ production cluster
   }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
